73495b3860618b1ad8e6e1a8e0aad2adeeb46473
sakurai <sakurai@g.pugpug.org>
2011-11-30 05:29:51 +0000

    fixed strange double-column layout

--- views/layouts/shop.php
+++ views/layouts/shop.php
@@ -29,40 +29,41 @@
 	<?php $this->widget('zii.widgets.CBreadcrumbs', array(
 		'links'=>$this->breadcrumbs,
 	)); ?><!-- breadcrumbs -->
-		<div id="mainmenu">
+	<div id="mainmenu">
 		<?php
-	$items = array();
-	$items[] = array('label'=>'Home', 'url'=>array('/site/index'));
-	$items[] = array('label'=>'All', 'url'=>array('/shop/products/index'));
-	foreach(Category::model()->findAll() as $category)
-	$items[] = array(
+		$items = array();
+		$items[] = array('label'=>'Home', 'url'=>array('/site/index'));
+		$items[] = array('label'=>'All', 'url'=>array('/shop/products/index'));
+		foreach(Category::model()->findAll() as $category)
+		$items[] = array(
 			'label' => $category->title,
 			'url' => array(
 				'//shop/category/view', 'id' => $category->category_id));
-	$items[] = array('label'=>'Admin', 'url'=>array('/shop/shop/admin'));
+		$items[] = array('label'=>'Admin', 'url'=>array('/shop/shop/admin'));
 
- $this->widget('zii.widgets.CMenu',array(
+		$this->widget('zii.widgets.CMenu',array(
 			'items'=>$items,
 		)); ?>
 	</div><!-- mainmenu -->
 
-	<div id="content">
-	<div style="float: right; max-height: 200px; width: 200px; margin: 5px;">
-	<?php
-	$this->widget('ShoppingCartWidget'); 
-	$this->widget('ProductCategoriesWidget'); 
-	if(!Yii::app()->user->isGuest) 
-		$this->widget('AdminWidget');
+	<div class="span-19">
+		<div id="content">
+   		<?php echo $content; ?>
+	        </div><!-- content -->
+	</div><!-- span-19 -->
 
-	?>	
-	</div>
+	<div class="span-5 last">
+		<div id="sidebar">
+		<?php
+		$this->widget('ShoppingCartWidget'); 
+		$this->widget('ProductCategoriesWidget'); 
+		if(!Yii::app()->user->isGuest) 
+			$this->widget('AdminWidget');
+		?>	
+		</div><!-- sidebar -->
+	</div><!-- span-5 -->
 
-	<div style="width: 700px;">
-	<?php echo $content; ?>
-	</div>
-	</div>
-
-	<div style="clear: both;"></div>
+	<div style="clear;"></div>
 
 	<div id="footer">
 		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
